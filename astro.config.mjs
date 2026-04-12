a6d99bf6f157eb434357b4a458dbd66955eb24a2
MiO <m.x0w0x.7@gmail.com>
2026-04-12 15:26:17 +0900

    Chore: setting files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: 'preserve',
+  },
+  server: {
+    host: true,
+    open: '/',
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  compressHTML: false,
+  integrations: [relativeLinks()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@layouts': '/src/layouts',
+        '@lib': '/src/lib',
+        '@scss': '/src/scss',
+        '@Js': '/src/js',
+        '@Images': '/src/images',
+      },
+    },
+  },
+  base: '/',
+});
